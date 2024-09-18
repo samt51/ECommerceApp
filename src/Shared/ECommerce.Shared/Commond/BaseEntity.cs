c9ada3766d2170bebc55f098a74dfc019ad05ba8
EMİRHAN BAĞLAN <samt51.m@icloud.com>
2024-09-18 19:58:31 +0300

    Add project files.

---
+++ src/Shared/ECommerce.Shared/Commond/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.Shared.Commond
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? ModifyDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
